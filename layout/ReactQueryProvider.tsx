2ea4b5b7be7028ac7b855a3707326a6fcb1ed967
Nguyễn Huy Hoàng <101138030+CallMeKennn@users.noreply.github.com>
2025-08-03 13:29:55 +0700

    Update ReactQueryProvider.tsx (Fix Query Client)

--- layout/ReactQueryProvider.tsx
+++ layout/ReactQueryProvider.tsx
@@ -4,13 +4,18 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
-
 const ReactQueryProvider = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+
+const queryClientRef = useRef<QueryClient>();
+
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+  
   return (
     <QueryClientProvider client={queryClient}>
       {children}
